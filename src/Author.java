31da5dfd85627cbc0ff9219e52cfa1543d25c5b8
Roman <filinroman79@gmail.com>
2024-11-26 23:44:25 +0300

    HW13-26.11.24

--- src/Author.java
+++ src/Author.java
@@ -26,5 +26,17 @@ public class Author {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
 }
 
