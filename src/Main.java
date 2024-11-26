59a3385ff73c9478e578a69e830e10a79a72398d
Roman <filinroman79@gmail.com>
2024-11-26 17:23:13 +0300

    HW12.1.1br

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author chehov = new Author("Anton", "Chehov" );
+        Author bulgakov = new Author("Mikhail", "Bulgakov" );
+        Book svadba = new Book("Свадьба", chehov, 1889);
+        Book masterAndMargarita = new Book("Мастер и Маргарита", bulgakov, 1967);
+
+        System.out.println(svadba);
+        System.out.println(masterAndMargarita);
+
+
+        svadba.setYear(1234);
+        System.out.println(masterAndMargarita);
+
     }
 }
